44541d01b69ffc1c8ef0009ade889269d2f83c37
Branislav Pilnan <B.Pilnan@gmail.com>
2020-04-11 21:27:44 +0100

    Create a python package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='classy_ppo',
+      version='0.0.1',
+      install_requires=[
+            'numpy',
+            'matplotlib',
+            'spinup',
+            'torch'
+      ]
+)
